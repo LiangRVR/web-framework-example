283ade92e248ad38b5fca62bd3e871e15e1542a7
LiangRVR <liangvillarrubia95@gmail.com>
2022-09-05 15:25:30 -0400

    refactor: implement Eventing clas to handle events; implement fetch and save user data in db

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "Liang", age: 26 });
+user.save();
+user.set({ name: "Lolo" });
 
-console.log(user.get("name"), user.get("age"));
-
-
-console.log(user)
+setTimeout(() => {
+  user.save();
+}, 8000);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,15 @@
+import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
-type CallBack = () => void;
-
 export class User {
-  events: { [key: string]: CallBack[] } = {};
+  public events: Eventing = new Eventing();
+  
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -17,20 +20,25 @@ export class User {
     Object.assign(this.data, user);
   }
 
-  on(eventName: string, callback: CallBack): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
   }
 
-  trigger(eventName: string) {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
+  save(): void {
+    const id = this.get("id");
+    console.log(id);
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios
+        .post(`http://localhost:3000/users`, this.data)
+        .then((response: AxiosResponse): void => {
+          this.set({ id: response.data.id });
+        });
     }
-
-    handlers.forEach((callback: CallBack): void => {
-      callback();
-    });
   }
 }
