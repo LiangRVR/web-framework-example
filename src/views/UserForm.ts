f56c454a875bf98a37deda41234e065f8c87ec9e
LiangRVR <liangvillarrubia95@gmail.com>
2022-09-08 15:28:35 -0400

    feat: implement eventsMap and bindEvents to add events to elementes in template

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,13 +1,40 @@
 export class UserForm {
   constructor(public parent: Element | null) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick,
+      "mouseenter:h1": this.onHeaderHover,
+    };
+  }
+
+  onHeaderHover(): void {
+    console.log("hover");
+  }
+
+  onButtonClick(): void {
+    console.log("button");
+  }
+
   template(): string {
     return `
             <div>
                 <h1>User Form</h1>
                 <input />
+                <button>Click Me</button>
             </div>
         `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+      fragment.querySelectorAll(selector).forEach((element: Element): void => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
@@ -16,6 +43,8 @@ export class UserForm {
     }
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
